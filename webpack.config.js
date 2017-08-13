b5b649f43aba21bfe747d0142f837060e33e440e
Ryan Tsang <ryantsangai@gmail.com>
2017-08-13 14:24:36 +0800

    fix production build error

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: process.env.NODE_ENV,
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
       }
     }),
     new ExtractTextPlugin("css/main.css"),
@@ -108,9 +108,13 @@ if (process.env.NODE_ENV === 'production') {
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.optimize.UglifyJsPlugin({
-      // compress: {
-      //   warnings: false
-      // }
+      compress: {
+        screw_ie8: true,
+        warnings: false
+      },
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
     })
   ])
 }
