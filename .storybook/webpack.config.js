8200ffb97b6c6be9ae4407fa885b9b58df8bc252
Ryan Tsang <ryantsangai@gmail.com>
2017-08-14 23:58:07 +0800

    fix build storybook error

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,24 +4,39 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin")
 const _ = require('lodash')
 
 module.exports = (storybookBaseConfig, configType) => {
-  storybookBaseConfig.module.rules.push({
-    test: /\.(css|scss)$/,
-    use: [
-      { loader: 'style-loader',},
-      { loader: 'css-loader', },
-      { loader: 'postcss-loader', },
-      { loader: 'sass-loader', }
-    ]
-  })
-  storybookBaseConfig.module.rules.push({
-    test: /\.sass$/,
-    use: [
-      { loader: 'style-loader',},
-      { loader: 'css-loader', },
-      { loader: 'postcss-loader', },
-      { loader: 'sass-loader?indentedSyntax', }
-    ]
-  })
+  storybookBaseConfig.module.rules = storybookBaseConfig.module.rules.concat([
+    {
+      test: /\.(css|scss)$/,
+      use: [
+        { loader: 'style-loader',},
+        { loader: 'css-loader', },
+        { loader: 'postcss-loader', },
+        { loader: 'sass-loader', }
+      ]
+    }, {
+      test: /\.sass$/,
+      use: [
+        { loader: 'style-loader',},
+        { loader: 'css-loader', },
+        { loader: 'postcss-loader', },
+        { loader: 'sass-loader?indentedSyntax', }
+      ]
+    }, {
+      test: /\.(png|jpe?g|gif)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: 'images/[name].[ext]',
+      }
+    }, {
+      test: /\.(woff2?|eot|ttf|otf|svg)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: 'fonts/[name].[ext]',
+      }
+    },
+  ])
 
   let vueLoader = _.find(storybookBaseConfig.module.rules, rule => String(rule.test) == String(/\.vue$/) )
   vueLoader.options = {
